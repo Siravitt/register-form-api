eea33c44c151e5e896ed23e02e927188d3bf7787
Siravit Tanratvijit <siravittanratvijit@Siravits-MacBook-Air.local>
2023-04-10 17:23:48 +0700

    initial project

---
+++ src/models/user.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+    },
+    {
+      underscored: true,
+    }
+  );
+
+  return User;
+};
